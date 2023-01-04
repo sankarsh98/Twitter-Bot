c908e29fb67383b1f2055a57ab6ca2c635562d85
sankarsh98 <sankarshpallela@gmail.com>
2023-01-05 00:24:56 +0530

    Updated deprecated methods and few xpaths

--- twitter_bot.py
+++ twitter_bot.py
@@ -3,9 +3,11 @@ import time
 from selenium import webdriver
 from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+
 
 # path to chrome driver, download it from https://sites.google.com/a/chromium.org/chromedriver/downloads
-chrome_driver = "/Users/sanky/Downloads/chromedriver"
+chrome_driver = "/chromedriver"
 
 
 class TwitterBot:
@@ -21,41 +23,44 @@ class TwitterBot:
 
         self.browser.get("https://www.twitter.com/login")
         time.sleep(5)
-        usernameInput = self.browser.find_element_by_name('text')
+        usernameInput = self.browser.find_element(By.NAME,'text')
         usernameInput.send_keys(self.username)
         time.sleep(3)
 
         self.browser.maximize_window()
 
-        nextButton1 = self.browser.find_element_by_xpath('//span[.="Next"]')
+        nextButton1 = self.browser.find_element(By.XPATH,'//span[.="Next"]')
         nextButton1.click()
         time.sleep(3)
 
         try:
-            middleInput = self.browser.find_element_by_name('text')
+            middleInput = self.browser.find_element(By.NAME,'text')
             middleInput.send_keys(self.mobile)
             time.sleep(3)
-            nextButton2 = self.browser.find_element_by_xpath('//span[.="Next"]')
+            nextButton2 = self.browser.find_element(By.XPATH,'//span[.="Next"]')
+            
             nextButton2.click()
             time.sleep(3)
-        except NoSuchElementException:
-            print ("exception")
+        except NoSuchElementException as nsee:
+            print ("exception : ",nsee)
 
-        passwordInput = self.browser.find_element_by_xpath('//input[@name="password"]')
+        passwordInput = self.browser.find_element(By.XPATH,'//input[@name="password"]')
         passwordInput.send_keys(self.password)
         # passwordInput.send_keys(Keys.ENTER)
         time.sleep(5)
 
-        loginButton = self.browser.find_element_by_xpath('//span[.="Log in"]')
+        loginButton = self.browser.find_element(By.XPATH,'//span[.="Log in"]')
         loginButton.click()
 
 
     def TweetSomething(self, message):
 
         time.sleep(5)
-        tweet_button = self.browser.find_element_by_xpath('//span[.="Tweet"]')
+        tweet_button = self.browser.find_element(By.XPATH,'//a[@aria-label="Tweet"]')
         tweet_button.click()
 
-        tweet = self.browser.find_element_by_xpath('//div[@aria-label="Tweet text"]')
-        tweet.send_keys(message)
-        tweet.send_keys(Keys.COMMAND, Keys.ENTER)
+        tweet_text = self.browser.find_element(By.XPATH,'//div[@aria-label="Tweet text"]')
+        tweet_text.send_keys(message)
+
+        tweet = self.browser.find_element(By.XPATH,'//span[.="Tweet"]')
+        tweet.click()
